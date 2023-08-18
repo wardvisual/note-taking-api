88cf5f6e2c66122a175a274f973a33f68439feb5
wardvisual <wardvisual@gmail.com>
2023-08-18 12:05:16 +0800

    create helpers

--- src/shared/constants/env.constant.ts
+++ src/shared/constants/env.constant.ts
@@ -3,11 +3,22 @@ import { config } from "dotenv";
 config();
 
 interface Env {
-  PORT: number;
+  APP_PORT: number;
+  APP_MONGODB_HOST: string;
+  APP_MONGODB_PORT: number;
+  APP_MONGODB_NAME: string;
 }
 
 const envs = process.env;
 
-export const { PORT }: Env = {
-  PORT: Number(envs.PORT),
+export const {
+  APP_PORT,
+  APP_MONGODB_HOST,
+  APP_MONGODB_PORT,
+  APP_MONGODB_NAME,
+}: Env = {
+  APP_PORT: Number(envs.APP_PORT),
+  APP_MONGODB_HOST: envs.APP_MONGODB_HOST,
+  APP_MONGODB_PORT: Number(envs.APP_MONGODB_PORT),
+  APP_MONGODB_NAME: envs.APP_MONGODB_NAME,
 };
