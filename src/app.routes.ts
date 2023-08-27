04d737f880cf3050ea18a53995fe34bddea51be1
wardvisual <wardvisual@gmail.com>
2023-08-27 11:57:33 +0800

    import notes module

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -1,10 +1,15 @@
 import { Module } from "@/astronautaking-api/types";
 
 import { UsersRoutes } from "./users/users.routes";
+import { NotesRoutes } from "./notes/notes.routes";
 
 export const routes: Module[] = [
   {
     path: "users",
     route: UsersRoutes,
+  },
+  {
+    path: "notes",
+    route: NotesRoutes,
   },
 ];
